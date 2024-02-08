12bd66e6930b3dfc8f9fda7bada68ba654f9c3ae
Sojeong <110121149+phinsso@users.noreply.github.com>
2024-02-08 17:06:03 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,13 +1,14 @@
-import java.math.*;
-
 class Solution {
     public String solution(String n_str) {
         String answer = "";
+        StringBuilder sb = new StringBuilder(n_str);
         
-        // BigInteger: 문자열 형태로 이루어져 있음.
-        // 정수로 변환 시에 숫자 앞에 있는 0은 자동으로 제거됨.
-        BigInteger str = new BigInteger(n_str);
-        answer = str.toString();
+        // 앞에서부터 제거 후 다시 while문을 실행하기 때문에 인덱스를 0으로
+        while(sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        
+        answer = sb.toString();
         
         return answer;
     }
